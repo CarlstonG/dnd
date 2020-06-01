44aed1fc5f3923c1149c394cb28e71e105bd2b5e
unknown <carlstongrefalde@gmail.com>
2020-06-01 17:45:36 +0800

    Updates

---
+++ precache-manifest.40d32bebc63afe2d31fa5b57a78071d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f39fb1b61d10c67b3f3b53cfaa864fe",
+    "url": "/dnd/index.html"
+  },
+  {
+    "revision": "f33a5bfcc1934b6b590f",
+    "url": "/dnd/static/css/main.d724bb72.chunk.css"
+  },
+  {
+    "revision": "9dbda4b062d94f280232",
+    "url": "/dnd/static/js/2.7b6798e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd/static/js/2.7b6798e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33a5bfcc1934b6b590f",
+    "url": "/dnd/static/js/main.9f14890f.chunk.js"
+  },
+  {
+    "revision": "b99e38fc891ec2e4fe31",
+    "url": "/dnd/static/js/runtime-main.682dd905.js"
+  }
+]);
